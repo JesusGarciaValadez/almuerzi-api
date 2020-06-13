37b1255d40740f58d39a89e3a1d24dcc1def853e
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-06-12 21:13:56 -0500

    feat: add netlify serverless utilities

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
+'use strict'
 const app = require('express')()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const serverless = require('serverless-http')
 const config = require('./config/index')
 const meals = require('./routes/meals')
 const orders = require('./routes/orders')
@@ -11,9 +13,9 @@ mongoose.connect(config.DB_URI, { useNewUrlParser: true, useUnifiedTopology: tru
 
 app.use(bodyParser.json())
 app.use(cors())
-app.use('/api/meals', meals)
-app.use('/api/orders', orders)
-app.use('/api/auth', auth)
-app.listen(3000)
+app.use('/.netlify/functions/api/meals', meals)
+app.use('/.netlify/functions/api/orders', orders)
+app.use('/.netlify/functions/api/auth', auth)
 
 module.exports = app;
+module.exports.handler = serverless(app)
